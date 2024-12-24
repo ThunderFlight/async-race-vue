b6c1747d9646b44d85f34274f17f85cf52c4b829
ThunderFlight <igoreshkin2006@gmail.com>
2024-12-24 02:09:30 +0200

    init: start task async-race

---
+++ async-race/src/modules/main/components/ControlForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form action="">
+    <div>
+      <button>race</button>
+      <button>restart</button>
+    </div>
+    <div>
+      <input type="text" placeholder="type car brand" />
+      <input type="color" />
+      <button>create</button>
+    </div>
+    <div>
+      <input type="text" placeholder="type car brand" />
+      <input type="color" />
+      <button>update</button>
+    </div>
+    <button>generate cars</button>
+  </form>
+</template>
+
+<style scoped></style>
---
+++ async-race/src/modules/main/main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ControlForm from "./components/ControlForm.vue";
+</script>
+
+<template>
+  <nav>
+    <button></button>
+    <button></button>
+  </nav>
+  <ControlForm />
+</template>
+
+<stype scoped></stype>
